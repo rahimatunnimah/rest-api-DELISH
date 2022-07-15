f352b7c10851503f5651fff31be5ef41f4501d92
rahimatunnimah <rahimatunnimah@gmail.com>
2022-07-15 10:01:13 +0700

    add branch deploy

--- config/db.js
+++ config/db.js
@@ -1,14 +1,25 @@
 /* eslint-disable no-undef */
-const Postgre = require("pg").Pool;
+const { Pool, Client } = require("pg");
 require("dotenv").config();
 
-const connection = new Postgre({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let connection;
+if (process.env.ENV_MODE === "prod") {
+  connection = new Client({
+    connectionString: process.env.DB_URI,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  connection = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
+
 const connect = async () => {
   try {
     const response = await connection.connect();
