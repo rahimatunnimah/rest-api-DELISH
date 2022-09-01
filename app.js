7635d21b417a555076ff92236923f20c0ea3fdaf
rahimatunnimah <rahimatunnimah@gmail.com>
2022-09-01 21:55:05 +0700

    connect to db uri

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ const corsOptionsDelegate = function (req, callback) {
 };
 
 app.use("/api", cors(corsOptionsDelegate), router);
+app.use("*", (req, res) => {
+  res.send("Sukses");
+});
 
 app.listen(port, function (err) {
   if (err) throw err;
