6c9a86278e251227bc7126ecb9fbccbca566dd64
rahimatunnimah <rahimatunnimah@gmail.com>
2022-09-05 21:34:27 +0700

    uncomment allowlist cors

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("images"));
 
 app.use(cors());
-// const allowlist = ["https://localhost:3000"];
-// const corsOptionsDelegate = function (req, callback) {
-//   let corsOptions;
-//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
-//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false }; // disable CORS for this request
-//   }
-//   callback(null, corsOptions); // callback expects two parameters: error and options
-// };
+const allowlist = ["https://localhost:3000"];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 app.use("/api", cors(corsOptionsDelegate), router);
 app.use("*", (req, res) => {
