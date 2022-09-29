030d5f53d4b4dfdd34cbb9d443ccfd772b9eb2da
rahimatunnimah <rahimatunnimah@gmail.com>
2022-09-29 13:02:24 +0700

    edit profile

--- middlewares/uploadProfile.js
+++ middlewares/uploadProfile.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./images/userProfile/");
+    cb(null, "./images");
   },
   filename: function (req, file, cb) {
     cb(
